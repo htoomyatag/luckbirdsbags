5454a3200a1ff816d9a142dbbb98f897a1e64025
htoomyataung <htoomyatag.webdev@gmail.com>
2016-07-07 15:11:49 +0630

    kick start

---
+++ config/initializers/assets.rb
@@ -0,0 +1,30 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+
+Rails.application.config.assets.precompile += %w(modernizr.js)
+Rails.application.config.assets.precompile += %w(modernizr-2.6.2.min.js)
+Rails.application.config.assets.precompile += %w(bootstrap.min.js)
+Rails.application.config.assets.precompile += %w(jquery.min.js)
+Rails.application.config.assets.precompile += %w(jquery.mobile.customized.min.js)
+Rails.application.config.assets.precompile += %w(jquery.easing.1.3.js)
+Rails.application.config.assets.precompile += %w(camera.js)
+Rails.application.config.assets.precompile += %w(velocity.min.js)
+Rails.application.config.assets.precompile += %w(main.nav.js)
+Rails.application.config.assets.precompile += %w(cbpFWTabs.js)
+Rails.application.config.assets.precompile += %w(numscroller-1.0.js)
+Rails.application.config.assets.precompile += %w(jquery.inview.min.js)
+Rails.application.config.assets.precompile += %w(wow.min.js)
+Rails.application.config.assets.precompile += %w(mousescroll.js)
+Rails.application.config.assets.precompile += %w(main.js)
+Rails.application.config.assets.precompile += %w(move-top.js)
+Rails.application.config.assets.precompile += %w(easing.js)
