79d2356eead3e376d917c966be9799e4e06a33ac
htoomyataung <htoomyatag.webdev@gmail.com>
2016-08-24 06:32:24 +0630

    aok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-        config.assets.compile = true
-      config.assets.digest = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.js_compressor = :uglifier
+  config.assets.digest = true
 
 end
